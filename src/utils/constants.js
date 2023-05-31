0e068bae814b1273f3ec1f11dd356d1eeb24f13a
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-05-31 19:25:23 +0600

    🏷️ [utils] constants added

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import CodeIcon from '@mui/icons-material/Code';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import HomeIcon from '@mui/icons-material/Home';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SchoolIcon from '@mui/icons-material/School';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon /> },
+  { name: 'JS Mastery', icon: <CodeIcon /> },
+  { name: 'Coding', icon: <CodeIcon /> },
+  { name: 'ReactJS', icon: <CodeIcon /> },
+  { name: 'NextJS', icon: <CodeIcon /> },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon /> },
+  { name: 'Podcast', icon: <GraphicEqIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon /> },
+  { name: 'Gaming', icon: <SportsEsportsIcon /> },
+  { name: 'Live', icon: <LiveTvIcon /> },
+  { name: 'Sport', icon: <FitnessCenterIcon /> },
+  { name: 'Fashion', icon: <CheckroomIcon /> },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon /> },
+  { name: 'Comedy', icon: <TheaterComedyIcon /> },
+  { name: 'Gym', icon: <FitnessCenterIcon /> },
+  { name: 'Crypto', icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/8mAITcNt710/maxresdefault.jpg';
+export const demoChannelUrl = '/channel/UC8butISFwT-Wl7EV0hUK0BQ';
+export const demoVideoUrl = '/video/8mAITcNt710';
+export const demoChannelTitle = 'FreeCodeCam.org';
+export const demoVideoTitle =
+  'Harvard CS50 – Full Computer Science University Course';
+export const demoProfilePicture =
+  'http://dergipark.org.tr/assets/app/images/buddy_sample.png';
