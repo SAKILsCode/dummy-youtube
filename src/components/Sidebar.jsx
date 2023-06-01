758bb22a4f2f03ca6ce624ab3dc75a55a7c10bcf
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-06-01 23:06:03 +0600

    🚧 Sidebar created

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const selectedCategory = 'New';
+
+const Sidebar = () => {
+  return (
+    <Stack
+      sx={{
+        overflow: 'auto',
+        height: { sx: 'auto', md: '95%' },
+        flexDirection: { md: 'column' },
+      }}
+    >
+      {categories.map((category) => {
+        const Icon = category.icon;
+        return (
+          <button
+            className='category-btn'
+            style={{
+              background: category.name === selectedCategory && '#FC1503',
+              color: 'white',
+            }}
+            key={category.name}
+          >
+            <span
+              style={{
+                color: category.name === selectedCategory ? 'white' : 'red',
+                marginRight: '15px',
+              }}
+            >
+              {<Icon />}
+            </span>
+            <span
+              style={{
+                opacity: category.name === selectedCategory ? '1' : '0.8',
+              }}
+            >
+              {category.name}
+            </span>
+          </button>
+        );
+      })}
+    </Stack>
+  );
+};
+export default Sidebar;
