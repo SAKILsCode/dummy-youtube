4e102f744ec95e0dcb8e9cee9820937db232bc94
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-06-09 11:19:22 +0600

    🚧 Searchbar and Search feed modified

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,6 +1,33 @@
+import { Box, Typography } from '@mui/material';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Videos } from '../components';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
-  )
-}
-export default SearchFeed
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography
+        variant='h4'
+        fontWeight='bold'
+        mb={2}
+        sx={{ color: '#f3f3f3' }}
+      >
+        Search Results for:{' '}
+        <span style={{ color: '#F31503' }}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+export default SearchFeed;
