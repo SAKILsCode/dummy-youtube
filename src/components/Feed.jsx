e47952c1c7300cc274f82b24aa6f40d9f178b43e
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-06-09 11:10:58 +0600

    🚧 Feed updated - videos added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,20 @@
 import { Box, Stack, Typography } from '@mui/material';
 
-import Sidebar from './Sidebar';
+import { useEffect, useState } from 'react';
+import { Sidebar, Videos } from '../components';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => {
+        setVideos(data.items)
+      })
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       <Box
@@ -12,7 +24,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className='copyright'
           variant='body2'
@@ -20,6 +35,18 @@ const Feed = () => {
         >
           Copyright 2023 - SAKILsCode
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: '#f3f3f3' }}
+        >
+          {selectedCategory} <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
