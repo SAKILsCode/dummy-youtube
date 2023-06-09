a674851a3ec901b881096feddd6bfa1157731a18
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-06-09 11:22:17 +0600

    🚧 ChannelCard and ChannelDetail updated

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,45 @@
+import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import ChannelCard from './ChannelCard';
+import Videos from './Videos';
+
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();[]
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
   return (
-    <div>ChannelDetail</div>
-  )
-}
-export default ChannelDetail
+    <Box sx={{ minHeight: '95vh' }}>
+      <Box>
+        <div
+          style={{
+            background:
+              'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(2,18,86,1) 23%, rgba(2,31,121,1) 48%, rgba(0,80,255,1) 100%)',
+            zIndex: 10,
+            height: '300px',
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-120px' />
+      </Box>
+
+      <Box display='flex' p={2}>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+export default ChannelDetail;
