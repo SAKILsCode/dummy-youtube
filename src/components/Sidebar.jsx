1c08f1bbb0c52b307548a5f132097a1ba0b64957
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-06-09 11:14:11 +0600

    🚧 Sidebar modified

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,7 @@
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
-
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
   return (
     <Stack
       sx={{
@@ -17,6 +15,7 @@ const Sidebar = () => {
         return (
           <button
             className='category-btn'
+            onClick={() => setSelectedCategory(category.name)}
             style={{
               background: category.name === selectedCategory && '#FC1503',
               color: 'white',
